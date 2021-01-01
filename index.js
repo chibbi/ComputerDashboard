b28ce2f250aab1e2d2fabb8174732f57e5904d86
chibbi <chehab.djibril@gmail.com>
2021-01-01 19:16:43 +0100

    some missing files

--- index.js
+++ index.js
@@ -5,16 +5,6 @@ const http = require("http").createServer(app);
 const httpport = 3000;
 
 const log = require("./logging")();
-log.log("Loaded logging: " + "5", 5);
-
-const myArgs = process.argv.slice(2);
-if (myArgs.includes("production")) {
-    log.printlog("Started as Production", 2);
-    process.env['NODE_ENV'] = "production";
-} else {
-    log.printlog("Started as DEVELOPMENT", 2);
-    process.env['NODE_ENV'] = "development";
-}
 
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
@@ -27,33 +17,6 @@ app.use(function (req, res, next) {
     next()
 });
 
-/* REQUEST LOGGING */
-let useramount;
-let userip = [];
-
-app.use(morgan(function (tokens, req, res) {
-    var curip = req.ip.slice(0, -3);
-    var exists = false;
-    if (userip.length != 0) {
-        for (var i = 0; i < userip.length; i++) {
-            if (curip == userip[i]) {
-                exists = true;
-                break;
-            }
-        }
-        if (!exists) {
-            userip.push(curip);
-        }
-    } else { userip.push(curip); }
-    useramount++;
-}, {
-    skip: function (req, res) {
-        if (req.url.includes(".css") || req.url.includes(".png") ||
-            req.url.includes(".js") || req.url.includes("api") ||
-            req.url == "/favicon.ico") { return false; }
-    }
-}));
-
 app.use(morgan(function (tokens, req, res) {
     log.printlog([tokens['remote-addr'](req, res),
     tokens.method(req, res),
@@ -61,30 +24,19 @@ app.use(morgan(function (tokens, req, res) {
     tokens.status(req, res), '-',
     tokens['total-time'](req, res), 'ms'
     ].join(' '), 4);
+}));
+app.use(morgan(function (tokens, req, res) {
+    log.printlog([tokens['remote-addr'](req, res),
+    tokens.method(req, res),
+    tokens.url(req, res),
+    tokens.status(req, res), '-',
+    tokens['total-time'](req, res), 'ms'
+    ].join(' '), 3);
 }, {
-    skip: function (req, res) {
-        if (false) { return true; }
-    }
+    skip: function (req, res) { return res.statusCode < 400 }
 }));
 
-if (process.env.NODE_ENV == "production") {
-    // log only 4xx and 5xx responses to console
-    app.use(morgan('combined', {
-        skip: function (req, res) { return res.statusCode < 400 }
-    }))
-} else {
-    app.use(morgan(function (tokens, req, res) {
-        log.printlog([tokens['remote-addr'](req, res),
-        tokens.method(req, res),
-        tokens.url(req, res),
-        tokens.status(req, res), '-',
-        tokens['total-time'](req, res), 'ms'
-        ].join(' '), 4);
-    }))
-}
-
 app.set('view engine', 'pug');
-
 /* ROUTER */
 require("./router")(app);
 
--- router.js
+++ router.js
@@ -11,6 +11,9 @@ module.exports = function(app) {
     app.get("/js/default.js", (req, res) => {
         res.sendFile(__dirname + "/static/js/default.js");
     });
+    app.get("/js/getData.js", (req, res) => {
+        res.sendFile(__dirname + "/static/js/getData.js");
+    });
     app.get("/css/default.css", (req, res) => {
         res.sendFile(__dirname + "/static/css/default.css");
     });
@@ -19,11 +22,73 @@ module.exports = function(app) {
     });
 
     // pug CAN use normal HTML
-    app.get('/', function(req, res) {
-        if (req.cookies.lcl == "de") {
-            res.render(__dirname + "/static/index.pug", lclde);
+    app.get('/', function (req, res) {
+        res.redirect("/login");
+    })
+
+    app.get('/login', function (req, res) {
+        if (req.cookies.session == undefined) {
+            res.sendFile(__dirname + "/static/login.html");
+        }
+        var [loggedin, user] = cloud.isLoggedIn(req.cookies.session);
+        if (loggedin) {
+            res.redirect("/home");
         } else {
-            res.render(__dirname + "/static/index.pug", lclen);
+            res.sendFile(__dirname + "/static/login.html");
+        }
+    })
+
+    app.post('/login', upload.none(), function (req, res) {
+        var loggedin = false;
+        var user = req.body.title;
+        var userpw = req.body.text;
+        if (user == undefined || userpw == undefined) {
+            res.sendFile(__dirname + "/static/login.html");
+        } else {
+            // TODO: hash those password :: https://www.toptal.com/nodejs/secure-rest-api-in-nodejs
+            var usersJsonFile = JSON.parse(fs.readFileSync(__dirname + "/userDB/users.json", "utf8"));
+            for (var i = 0; i < Object.keys(usersJsonFile).length; i++) {
+                var element = usersJsonFile[i];
+                if (user == element.name && userpw == element.password) {
+                    loggedin = true;
+                    log.log(user, 4);
+                }
+            }
+            if (loggedin) {
+                res.cookie('session', cloud.createSession(user), {});
+                setTimeout(() => { }, 100);
+                res.redirect("/home");
+            } else {
+                res.sendFile(__dirname + "/static/login.html");
+            }
+        }
+    })
+
+    app.get('/home', function (req, res) {
+        if (req.cookies.session == undefined) {
+            res.redirect("/login");
+        }
+        var [loggedin, user] = cloud.isLoggedIn(req.cookies.session);
+        if (loggedin) {
+            if (req.cookies.lcl == "de") {
+                res.render(__dirname + "/static/index.pug", lclde);
+            } else {
+                res.render(__dirname + "/static/index.pug", lclen);
+            }
+        } else {
+            res.redirect("/login");
+        }
+    })
+
+    app.get('/api', function (req, res) {
+        if (req.cookies.session == undefined) {
+            res.redirect("/login");
+        }
+        var [loggedin, user] = cloud.isLoggedIn(req.cookies.session);
+        if (loggedin) {
+            // TODO send stast
+        } else {
+            res.redirect("/login");
         }
     })
     return module;
