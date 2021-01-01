f9278860a8861c5b0dbad060f1c90be6c13da97e
chibbi <chehab.djibril@gmail.com>
2021-01-01 19:48:58 +0100

    Started with the actual getting of stats

--- comp-stats.js
+++ comp-stats.js
@@ -1,21 +1,33 @@
 //
 // written by: chibbi
 //
-module.exports = function() {
+module.exports = function () {
     const fs = require("fs");
     const spawn = require('child_process').spawn
 
+    /*
+    Monitor sleep mode :: xset dpms force off
+    sleep mode :: systemctl suspend
+    hibernate mode :: systemctl hibernate // not working on my PC
+    shutdown mode :: shutdown 
+    */
     // Should get called every second
-    module.getStats = function() {
-        var cmd = spawn(nixJsonAPIScript, [pluginName, '']);
+    module.getStats = function () {
+        var stats = [];
+        var cpuInfo = getSpecificStat("cpu_info");
+        console.log(cpuInfo);
+        fs.writeFileSync(__dirname + "/userDB/users.json", Buffer.from(JSON.stringify(stats)), "utf8");
+        return true;
+    }
+
+    function getSpecificStat(funcName) {
+        var cmd = spawn("./linux_json_api.sh", [funcName, '']);
         var stats = [];
 
-        command.stdout.on('data', function(chunk) {
-            output.push(chunk.toString())
+        cmd.stdout.on('data', function (chunk) {
+            stats.push(chunk.toString())
         })
+        return stats;
     }
-    fs.writeFileSync(__dirname + "/userDB/users.json", Buffer.from(JSON.stringify(stats)), "utf8");
-    return true;
-}
 return module;
 }
